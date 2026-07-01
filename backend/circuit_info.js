f31c0a6a009eb8b5fe2d251b02a1d843bf7769cc
Jonathan <jskroppa@gmail.com>
2026-07-01 22:25:39 +0100

    Feat: Circuit info as none of the apis I want to use has all the data, so have decided to make my own lookup table.

---
+++ backend/circuit_info.js
@@ -0,0 +1,6 @@
+
+//TODO: Fill in the rest of this with data fo the rest of the races.
+const circuitInfo = {
+    silverstone:    { laps: 52, length: '5.892 km'},
+    spa:            { laps: 53, length: '5.793 km'}
+}
